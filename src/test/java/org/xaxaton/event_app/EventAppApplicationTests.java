f84d58e85c5dcc3a3403a9403492ef7bfdc6f551
Alexxeev <alexeev.dev@yandex.ru>
2025-03-29 11:20:34 +0300

    Initial commit

---
+++ src/test/java/org/xaxaton/event_app/EventAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xaxaton.event_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
